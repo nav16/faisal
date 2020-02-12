0fafdaee722c0c689285bfb23dec0f3a32df4d8d
Navneet Pandey <npandey057@gmail.com>
2020-02-12 13:37:46 +0530

    initial commit

---
+++ src/main/java/com/example/faisal/FaisalApplication.java
@@ -0,0 +1,13 @@
+package com.example.faisal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaisalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaisalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/faisal/FaisalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faisal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaisalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

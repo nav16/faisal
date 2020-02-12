9f96d2dc689fcb615070f50867ea5dc311d7ca7a
Navneet Pandey <npandey057@gmail.com>
2020-02-12 13:48:55 +0530

    add logging properties

--- src/main/java/com/example/faisal/FaisalApplication.java
+++ src/main/java/com/example/faisal/FaisalApplication.java
@@ -1,9 +1,18 @@
 package com.example.faisal;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@Slf4j
+@EnableJpaRepositories(basePackages = {"com.example.faisal.repositories"})
+@EnableTransactionManagement
+@PropertySource(value = "${spring.config.location:classpath}:application-${spring.profiles.active}.properties",
+        ignoreResourceNotFound = true)
 public class FaisalApplication {
 
     public static void main(String[] args) {
